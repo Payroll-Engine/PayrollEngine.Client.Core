7d2716530879fd79c08dc6459e6adb145fac5585
Jani Giannoudis <jani@giannoudis.ch>
2023-07-30 14:21:41 +0200

    refactored http configuration extension to shared configuration updated version to 0.6.0-beta.5

--- Client.Core/ConfigurationExtensions.cs
+++ Client.Core/ConfigurationExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace PayrollEngine.Client;
@@ -11,52 +9,15 @@ public static class ConfigurationExtensions
     /// <summary>Get the http configuration</summary>
     public static async Task<PayrollHttpConfiguration> GetHttpConfigurationAsync(this IConfiguration configuration)
     {
-        // priority 2: application configuration
-        var httpConfiguration = configuration.GetConfiguration<PayrollHttpConfiguration>();
-
         // priority 1: shared configuration
-        var sharedConfigFileName = Environment.GetEnvironmentVariable(SystemSpecification.PayrollConfigurationVariable);
-        if (string.IsNullOrWhiteSpace(sharedConfigFileName) || !File.Exists(sharedConfigFileName))
+        var httpConfiguration = await SharedHttpConfiguration.GetHttpConfigurationAsync();
+        if (httpConfiguration != null)
         {
             return httpConfiguration;
         }
-        var sharedConfig = await SharedConfiguration.ReadAsync();
-        var backendUrl = SharedConfiguration.GetSharedValue(sharedConfig, PayrollApiSpecification.BackendUrlSetting);
-        var backendPort = SharedConfiguration.GetSharedValue(sharedConfig, PayrollApiSpecification.BackendPortSetting);
-        var port = 0;
-        if (!string.IsNullOrWhiteSpace(backendPort))
-        {
-            port = int.Parse(backendPort);
-        }
 
-        if (httpConfiguration == null)
-        {
-            // http configuration from environment
-            if (!string.IsNullOrWhiteSpace(backendUrl) || port > 0)
-            {
-                httpConfiguration = new PayrollHttpConfiguration(backendUrl, port);
-            }
-        }
-        else
-        {
-            // merge config and environment configuration
-            if (string.IsNullOrWhiteSpace(httpConfiguration.BaseUrl) && !string.IsNullOrWhiteSpace(backendUrl))
-            {
-                httpConfiguration.BaseUrl = backendUrl;
-            }
-            if (httpConfiguration.Port == 0 && port > 0)
-            {
-                httpConfiguration.Port = port;
-            }
-        }
-
-        // test
-        if (httpConfiguration != null &&
-            (string.IsNullOrWhiteSpace(httpConfiguration.BaseUrl) || httpConfiguration.Port == 0))
-        {
-            return null;
-        }
-
+        // priority 2: application configuration
+        httpConfiguration = configuration.GetConfiguration<PayrollHttpConfiguration>();
         return httpConfiguration;
     }
 }
---
+++ Client.Core/SharedHttpConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PayrollEngine.Client;
+
+/// <summary>Http configuration from environment <see cref="SystemSpecification.PayrollConfigurationVariable"/></summary>
+public static class SharedHttpConfiguration
+{
+    /// <summary>Get the http configuration</summary>
+    public static async Task<PayrollHttpConfiguration> GetHttpConfigurationAsync()
+    {
+        var sharedConfigFileName = Environment.GetEnvironmentVariable(SystemSpecification.PayrollConfigurationVariable);
+        if (string.IsNullOrWhiteSpace(sharedConfigFileName) || !File.Exists(sharedConfigFileName))
+        {
+            return null;
+        }
+        var sharedConfig = await SharedConfiguration.ReadAsync();
+        var backendUrl = SharedConfiguration.GetSharedValue(sharedConfig, PayrollApiSpecification.BackendUrlSetting);
+        var backendPort = SharedConfiguration.GetSharedValue(sharedConfig, PayrollApiSpecification.BackendPortSetting);
+        var port = 0;
+        if (!string.IsNullOrWhiteSpace(backendPort))
+        {
+            port = int.Parse(backendPort);
+        }
+
+        if (string.IsNullOrWhiteSpace(backendUrl) || port <= 0)
+        {
+            return null;
+        }
+        return new PayrollHttpConfiguration(backendUrl, port);
+    }
+}
